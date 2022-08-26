6c4f155065aae81cb0649b03680fd092e9338bf1
johnbledsoe1986 <johnbledsoe1986@aol.com>
2022-08-25 20:40:57 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() *256);
+    const blue = Math.floor(Math.random() *256);
+
+    document.body.style.background = "rgb(" + red + ", " + green + ", " + blue + ")";
+}
